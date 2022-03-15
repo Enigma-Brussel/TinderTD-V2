0de241e09d633a03b1a5d48c580cf39d00e6aa1e
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2022-03-15 18:39:17 +0100

    Adding server url to fetch

--- app/src/public/script/main.js
+++ app/src/public/script/main.js
@@ -1,4 +1,4 @@
-const serverUrl =  "http://localhost:8000" // "https://tinder.enigmabrussel.be";
+const serverUrl =  "https://tinder.enigmabrussel.be" // "https://tinder.enigmabrussel.be";
 let setting = {};
 
 function $(x) {
