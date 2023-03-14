5938c4375c432617ab4cf20ca1ccf8861441f6e7
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2023-03-14 22:49:40 +0100

    preparing for 1st test deployment

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -343,8 +343,8 @@ class ComposerStaticInitc565f141cf74835c0a99998b09411a80
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '697a22009a7fbe1697249c25a2f7c3d3819feeed',
+        'reference' => '494ad7a3ec523c9f6bf3712606868805da25382d',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '697a22009a7fbe1697249c25a2f7c3d3819feeed',
+            'reference' => '494ad7a3ec523c9f6bf3712606868805da25382d',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -733,8 +733,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
