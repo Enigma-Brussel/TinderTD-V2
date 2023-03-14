697a22009a7fbe1697249c25a2f7c3d3819feeed
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2023-03-14 22:18:20 +0100

    preparing for 1st test deployment

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -27,8 +27,6 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
 
-        dd($request);
-
         $request->user()->fill($request->validated());
 
         if ($request->user()->isDirty('email')) {
